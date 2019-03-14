626cc19d1c760fc455fcc1be417a18a30cf45e39
Ash P <ashpeterson177@gmail.com>
2019-03-14 12:42:19 +0000

    Add project files.

---
+++ SecuroteckWebApplication/Controllers/Authorisation/CustomAuthoriseAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace SecuroteckWebApplication.Controllers
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class APIAuthoriseAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if(!Thread.CurrentPrincipal.Identity.IsAuthenticated) // If the principle.identity on the current thread is not authenticated
+            {
+                // Respond with an 'Unauthorised' status code and error
+                actionContext.Response = actionContext.ControllerContext.Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Unauthorized. Check ApiKey in Header is correct.");
+            }
+        }
+
+    }
+
+    #region Task7
+    // TODO: Add Admin role authorisation attribute
+    #endregion
+}
---
+++ SecuroteckWebApplication/Controllers/TalkBackController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SecuroteckWebApplication.Controllers
+{
+    public class TalkBackController : ApiController
+    {
+        [ActionName("Hello")]
+        public string Get()
+        {
+            #region TASK1
+            return "Hello World 123";
+            // TODO: add api/talkback/hello response
+            #endregion
+        }
+
+        [ActionName("Sort")]
+        public int[] Get([FromUri]int[] integers)
+        {
+            #region TASK1
+            try
+            {
+                var ints = integers.OrderBy(i => i).ToArray();
+                return ints;
+            }
+            catch (Exception)
+            {
+                throw;
+               // return new HttpResponseMessage(HttpStatusCode.NotModified);
+            }
+              
+
+            
+            
+            //return new HttpResponseMessage(HttpStatusCode.NotModified);
+            // TODO: 
+            // sort the integers into ascending order
+            // send the integers back as the api/talkback/sort response
+            #endregion
+        }
+
+    }
+}
