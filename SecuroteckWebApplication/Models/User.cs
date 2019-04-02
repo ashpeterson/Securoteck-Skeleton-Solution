6140b53968ccacda81335f8f5b56db08b9e954a0
Ash P <ashpeterson177@gmail.com>
2019-04-02 09:21:15 +0100

    Task 3.1 3.2

--- SecuroteckWebApplication/Models/User.cs
+++ SecuroteckWebApplication/Models/User.cs
@@ -13,6 +13,11 @@ namespace SecuroteckWebApplication.Models
     public class User
     {
         #region Task2
+        public enum Role
+        {
+            Admin,
+            User
+        }
 
         public User() { }
 
@@ -24,16 +29,10 @@ namespace SecuroteckWebApplication.Models
         public Role UserRole { get; set; }
     }
 
-    public enum Role
-    {
-        Admin,
-        User
-    }
+    // TODO: Create a User Class for use with Entity Framework
+    // Note that you can use the [key] attribute to set your ApiKey Guid as the primary key  
+    #endregion
 
-        // TODO: Create a User Class for use with Entity Framework
-        // Note that you can use the [key] attribute to set your ApiKey Guid as the primary key  
-        #endregion
-    
 
     #region Task13?
     // TODO: You may find it useful to add code here for Logging
@@ -43,35 +42,80 @@ namespace SecuroteckWebApplication.Models
     {
         #region Task3 
 
-        
-        public void Create (User user)
+        /// <summary>
+        /// Check if a user with a given ApiKey string exists in the database, returning true or false.
+        /// </summary>
+        /// <param name="userContext"></param>
+        /// <returns></returns>
+        public bool CheckIfApiKeyExists(UserContext userContext)
         {
-            UserContext ctx = new UserContext();                
+            User user = new User();
+            var id = new Guid();
 
-            if (ctx.Users.Add(o => o.ctx.Users == user))
+            if (userContext.Users.Any(o => o.ApiKey == id.ToString()))
             {
-                var id = new Guid();
-                using(var uc = new UserContext())
-                {
-                    user = new User { ApiKey = id.ToString(), UserName = "New User", UserRole = Role.User };              
-                }         
-                // Match!
+                return true;
             }
+            else
+            {
+                return false;
+            }
+        }
 
-        } 
+        public bool CheckIfApiKeyAndUserNameExist(UserContext userContext)
+        {
+            User user = new User();
+            var id = new Guid();
 
-        public void Read(User user)  {
+            if (userContext.Users.Any(o => o.ApiKey == id.ToString() + u => u.user == ())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Create a new user, using a username given as a parameter and creating a new GUID which is saved as a
+        /// string to the database as the ApiKey.This must return the ApiKey or the User object so that the server can
+        /// pass the Key back to the client.
+        /// </summary>
+        /// <param name="userContext"></param>
+        public void CreateNewUserIfNotExist(UserContext userContext)
+        {
+
+            User user = new User();
+            var id = new Guid();
+
+            if (userContext.Users.Any(o => o.ApiKey == id.ToString()))
+            {
+                return;
+            }
+            else
+            {
+                using (var uc = new UserContext())
+                {
+                    user = new User { ApiKey = id.ToString(), UserName = "", UserRole = User.Role.User };
+                }
+
+            }
+        }
+
+        public void ReturnUserIfExist(User user)
+        {
             using (UserContext ctx = new UserContext())
             {
                 var query = from b in ctx.Users orderby b.UserName select b;
-                foreach (var  in query)
+                foreach (var in query)
                 {
                     return user;
                 }
             }
-        }            
-        
+        }
+
     }
-     // TODO: Make methods which allow us to read from/write to the database 
-     #endregion
-} 
+    // TODO: Make methods which allow us to read from/write to the database 
+    #endregion
+}
